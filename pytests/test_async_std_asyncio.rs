5fb84e5b94021493e99a1f01fafcb9dbe93395d9
Andrew J Westlake <awestlake87@yahoo.com>
2021-01-25 07:04:04 -0600

    removed py argument from into_future

--- pytests/test_async_std_asyncio.rs
+++ pytests/test_async_std_asyncio.rs
@@ -40,7 +40,6 @@ fn test_into_coroutine(
     Ok(async move {
         Python::with_gil(|py| {
             pyo3_asyncio::into_future(
-                py,
                 test_mod
                     .call_method1(py, "sleep_for_1s", (sleeper_mod.getattr(py, "sleep_for")?,))?
                     .as_ref(py),
@@ -60,7 +59,7 @@ fn test_async_sleep<'p>(
         task::sleep(Duration::from_secs(1)).await;
 
         Python::with_gil(|py| {
-            pyo3_asyncio::into_future(py, asyncio.as_ref(py).call_method1("sleep", (1.0,))?)
+            pyo3_asyncio::into_future(asyncio.as_ref(py).call_method1("sleep", (1.0,))?)
         })?
         .await?;
 
