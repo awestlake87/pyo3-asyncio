ef247762be60c1032cfff98d830e1517e8e03fbd
Andrew J Westlake <awestlake87@yahoo.com>
2021-01-27 12:21:46 -0600

    Removed TODO comment since it's already been fixed

--- pytests/test_tokio_current_thread_asyncio.rs
+++ pytests/test_tokio_current_thread_asyncio.rs
@@ -1,7 +1,6 @@
 mod common;
 mod tokio_asyncio;
 
-// TODO: Fix current thread init
 pyo3_asyncio::testing::test_main!(
     #[pyo3_asyncio::tokio::main(flavor = "current_thread")],
     "PyO3 Asyncio Tokio Current Thread Test Suite"
