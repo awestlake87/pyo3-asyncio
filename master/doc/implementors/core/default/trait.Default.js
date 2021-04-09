aa58dad308020c341f599b9208dac08f3fb81324
awestlake87 <awestlake87@users.noreply.github.com>
2021-04-09 17:22:32 +0000

    Upload documentation for master

--- master/doc/implementors/core/default/trait.Default.js
+++ master/doc/implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["pyo3_asyncio"] = [{"text":"impl Default for Args","synthetic":false,"types":[]}];
+implementors["pyo3_asyncio"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"pyo3_asyncio/testing/struct.Args.html\" title=\"struct pyo3_asyncio::testing::Args\">Args</a>","synthetic":false,"types":["pyo3_asyncio::testing::Args"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
