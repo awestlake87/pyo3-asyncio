41b5929e60c2ab58c69be9c8821c3b6fe11b60f1
awestlake87 <awestlake87@users.noreply.github.com>
2021-09-26 12:41:55 +0000

    Upload documentation for master

--- master/doc/implementors/core/clone/trait.Clone.js
+++ master/doc/implementors/core/clone/trait.Clone.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["pyo3_asyncio"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"pyo3_asyncio/testing/struct.Test.html\" title=\"struct pyo3_asyncio::testing::Test\">Test</a>","synthetic":false,"types":["pyo3_asyncio::testing::Test"]}];
+implementors["pyo3_asyncio"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"pyo3_asyncio/testing/struct.Test.html\" title=\"struct pyo3_asyncio::testing::Test\">Test</a>","synthetic":false,"types":["pyo3_asyncio::testing::Test"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- master/doc/implementors/core/fmt/trait.Debug.js
+++ master/doc/implementors/core/fmt/trait.Debug.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["pyo3_asyncio"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"pyo3_asyncio/err/struct.RustPanic.html\" title=\"struct pyo3_asyncio::err::RustPanic\">RustPanic</a>","synthetic":false,"types":["pyo3_asyncio::err::exceptions::RustPanic"]}];
+implementors["pyo3_asyncio"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"pyo3_asyncio/err/struct.RustPanic.html\" title=\"struct pyo3_asyncio::err::RustPanic\">RustPanic</a>","synthetic":false,"types":["pyo3_asyncio::err::exceptions::RustPanic"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
