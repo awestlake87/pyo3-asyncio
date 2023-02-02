b3bcb0327c3f5d09dbc6997e373a880c0eb5cc09
awestlake87 <awestlake87@users.noreply.github.com>
2023-02-02 01:28:21 +0000

    Upload documentation for master

--- master/doc/implementors/pyo3/instance/trait.PyNativeType.js
+++ master/doc/implementors/pyo3/instance/trait.PyNativeType.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["pyo3_asyncio"] = [{"text":"impl PyNativeType for <a class=\"struct\" href=\"pyo3_asyncio/err/struct.RustPanic.html\" title=\"struct pyo3_asyncio::err::RustPanic\">RustPanic</a>","synthetic":false,"types":["pyo3_asyncio::err::exceptions::RustPanic"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"pyo3_asyncio":[["impl PyNativeType for <a class=\"struct\" href=\"pyo3_asyncio/err/struct.RustPanic.html\" title=\"struct pyo3_asyncio::err::RustPanic\">RustPanic</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- master/doc/implementors/pyo3_asyncio/generic/trait.JoinError.js
+++ master/doc/implementors/pyo3_asyncio/generic/trait.JoinError.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["pyo3_asyncio"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"pyo3_asyncio":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
