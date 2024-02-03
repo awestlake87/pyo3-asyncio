509b7f174adab6aa61102ccde24c397974064a78
Andrew J Westlake <kelixes@gmail.com>
2024-02-03 02:28:40 +0000

    Interned more commonly used strings

--- src/lib.rs
+++ src/lib.rs
@@ -411,27 +411,28 @@ static GET_RUNNING_LOOP: OnceCell<PyObject> = OnceCell::new();
 fn ensure_future<'p>(py: Python<'p>, awaitable: &'p PyAny) -> PyResult<&'p PyAny> {
     ENSURE_FUTURE
         .get_or_try_init(|| -> PyResult<PyObject> {
-            Ok(asyncio(py)?.getattr("ensure_future")?.into())
+            Ok(asyncio(py)?.getattr(pyo3::intern!(py, "ensure_future"))?.into())
         })?
         .as_ref(py)
         .call1((awaitable,))
 }
 
 fn create_future(event_loop: &PyAny) -> PyResult<&PyAny> {
-    event_loop.call_method0("create_future")
+    event_loop.call_method0(pyo3::intern!(event_loop.py(), "create_future"))
 }
 
 fn close(event_loop: &PyAny) -> PyResult<()> {
+    let py = event_loop.py();
     event_loop.call_method1(
-        "run_until_complete",
-        (event_loop.call_method0("shutdown_asyncgens")?,),
+        pyo3::intern!(py, "run_until_complete"),
+        (event_loop.call_method0(pyo3::intern!(py, "shutdown_asyncgens"))?,),
     )?;
 
     // how to do this prior to 3.9?
-    if event_loop.hasattr("shutdown_default_executor")? {
+    if event_loop.hasattr(pyo3::intern!(py, "shutdown_default_executor"))? {
         event_loop.call_method1(
-            "run_until_complete",
-            (event_loop.call_method0("shutdown_default_executor")?,),
+            pyo3::intern!(py, "run_until_complete"),
+            (event_loop.call_method0(pyo3::intern!(py, "shutdown_default_executor"))?,),
         )?;
     }
 
@@ -442,7 +443,7 @@ fn close(event_loop: &PyAny) -> PyResult<()> {
 
 fn asyncio(py: Python) -> PyResult<&PyAny> {
     ASYNCIO
-        .get_or_try_init(|| Ok(py.import("asyncio")?.into()))
+        .get_or_try_init(|| Ok(py.import(pyo3::intern!(py, "asyncio"))?.into()))
         .map(|asyncio| asyncio.as_ref(py))
 }
 
@@ -456,7 +457,7 @@ pub fn get_running_loop(py: Python) -> PyResult<&PyAny> {
         .get_or_try_init(|| -> PyResult<PyObject> {
             let asyncio = asyncio(py)?;
 
-            Ok(asyncio.getattr("get_running_loop")?.into())
+            Ok(asyncio.getattr(pyo3::intern!(py, "get_running_loop"))?.into())
         })?
         .as_ref(py)
         .call0()
@@ -464,12 +465,12 @@ pub fn get_running_loop(py: Python) -> PyResult<&PyAny> {
 
 fn contextvars(py: Python) -> PyResult<&PyAny> {
     Ok(CONTEXTVARS
-        .get_or_try_init(|| py.import("contextvars").map(|m| m.into()))?
+        .get_or_try_init(|| py.import(pyo3::intern!(py, "contextvars")).map(|m| m.into()))?
         .as_ref(py))
 }
 
 fn copy_context(py: Python) -> PyResult<&PyAny> {
-    contextvars(py)?.call_method0("copy_context")
+    contextvars(py)?.call_method0(pyo3::intern!(py, "copy_context"))
 }
 
 /// Task-local data to store for Python conversions.
@@ -528,8 +529,9 @@ struct PyTaskCompleter {
 impl PyTaskCompleter {
     #[pyo3(signature = (task))]
     pub fn __call__(&mut self, task: &PyAny) -> PyResult<()> {
-        debug_assert!(task.call_method0("done")?.extract()?);
-        let result = match task.call_method0("result") {
+        let py = task.py();
+        debug_assert!(task.call_method0(pyo3::intern!(py, "done"))?.extract()?);
+        let result = match task.call_method0(pyo3::intern!(py, "result")) {
             Ok(val) => Ok(val.into()),
             Err(e) => Err(e),
         };
@@ -560,7 +562,7 @@ impl PyEnsureFuture {
         Python::with_gil(|py| {
             let task = ensure_future(py, self.awaitable.as_ref(py))?;
             let on_complete = PyTaskCompleter { tx: self.tx.take() };
-            task.call_method1("add_done_callback", (on_complete,))?;
+            task.call_method1(pyo3::intern!(py, "add_done_callback"), (on_complete,))?;
 
             Ok(())
         })
@@ -575,9 +577,9 @@ fn call_soon_threadsafe(
     let py = event_loop.py();
 
     let kwargs = PyDict::new(py);
-    kwargs.set_item("context", context)?;
+    kwargs.set_item(pyo3::intern!(py, "context"), context)?;
 
-    event_loop.call_method("call_soon_threadsafe", args, Some(kwargs))?;
+    event_loop.call_method(pyo3::intern!(py, "call_soon_threadsafe"), args, Some(kwargs))?;
     Ok(())
 }
 
@@ -653,7 +655,7 @@ pub fn into_future_with_locals(
             Ok(item) => item,
             Err(_) => Python::with_gil(|py| {
                 Err(PyErr::from_value(
-                    asyncio(py)?.call_method0("CancelledError")?,
+                    asyncio(py)?.call_method0(pyo3::intern!(py, "CancelledError"))?,
                 ))
             }),
         }
